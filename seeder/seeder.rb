a4f8e26ce10062079c6d16bac0e8a6f9ce47d6a7
justinever9 <justinever9@gmail.com>
2023-11-20 21:28:42 +0700

    init to test browser fetch

---
+++ seeder/seeder.rb
@@ -0,0 +1,15 @@
+pages << {
+    url: "https://naivas.online/baby-kids?q=Categories-BABY+FEEDING",
+    page_type: "location",
+    fetch_type: 'browser',
+    headers: {
+        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+        'Accept-Encoding' => 'gzip, deflate, br',
+        'Accept-Language' => 'en-US,en;q=0.9',
+        'Accept' => 'application/json, text/javascript, */*; q=0.01',
+        'Origin' => 'https://naivas.online/',
+        'Referer' => 'https://naivas.online/baby-kids?q=Categories-BABY+FEEDING',
+        'X-Requested-With' => 'XMLHttpRequest',
+    },
+    method: "GET"
+}

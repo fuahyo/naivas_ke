8daacca603e3431a320617d74c6af4ee115a8772
justinever9 <justinever9@gmail.com>
2024-03-12 20:14:53 +0700

    update header

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -4,7 +4,7 @@ pages << {
     fetch_type: 'browser',
     method: "GET",
     headers: {
-        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+        'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         'Accept-Encoding' => 'gzip, deflate, br',
         'Accept-Language' => 'en-US,en;q=0.9',
         'Cache-Control' => 'max-age=0',
